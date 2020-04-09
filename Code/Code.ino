cd2e4cd672893fd0ae47ce7fca752f11f889e62d
Jakub Kajzer <38817191+JakubKajzer@users.noreply.github.com>
2020-04-09 13:44:56 +0200

    1.3.0
    Everythink works fine

--- Code/Code.ino
+++ Code/Code.ino
@@ -1,8 +1,9 @@
- #include <Adafruit_MCP23017.h>
+#include <Adafruit_MCP23017.h>
 #include <Wire.h>
 #include <TimerOne.h>
+#include <avr/sleep.h>
 
-#define DEBUG false
+#define DEBUG true
 
 
 #define SDI 12
@@ -22,38 +23,61 @@
 
  Adafruit_MCP23017 mcp;
 
-volatile int stateMachine=0;
+volatile byte stateMachine=0;
 volatile bool prevBtnState=true;
-volatile int btnState=true;
+volatile byte btnState=true;
 
-volatile short digits[6]={1,2,3,4,5,6};
-volatile uint32_t value=123456;
+short digits[6]={1,2,3,4,5,6};
+uint16_t value=64123;
 
-volatile short digitPosition =0;
+volatile byte digitPosition =0;
 
-volatile bool sdiData1[8];
-volatile bool sdiData2[8];
-volatile bool updateSwitchesFlag = true;
+volatile short timeToSleep = 0;
+
+unsigned int previousStateOfSwitches;
+
+  // E,D,DP,C,G,A,F,B
+  const bool lookup[18][8] = //for 7seg
+     {{1,1,0,1,0,1,1,1}, //0
+    {0,0,0,1,0,0,0,1}, //1
+    {1,1,0,0,1,1,0,1}, //2
+    {0,1,0,1,1,1,0,1}, //3
+    {0,0,0,1,1,0,1,1}, //4
+    {0,1,0,1,1,1,1,0}, //5
+    {1,1,0,1,1,1,1,0}, //6
+    {0,0,0,1,0,1,0,1}, //7
+    {1,1,0,1,1,1,1,1}, //8
+    {0,1,0,1,1,1,1,1}, //9
+    {1,0,0,1,1,1,1,1}, //A
+    {1,1,0,1,1,0,1,0}, //B
+    {1,1,0,0,0,1,1,0}, //C
+    {1,1,0,1,1,0,0,1}, //D
+    {1,1,0,0,1,1,1,0}, //E
+    {1,0,0,0,1,1,1,0}, //F
+    {0,0,0,0,0,0,0,0}, //NULL
+    {1,0,1,1,1,0,1,1}};//H for hexes
+
+
 void setup() 
 {
-  
+  //POWER SAVE ////////
+  ADCSRA = 0x00; // turn off ADC to save power
   if(DEBUG) Serial.begin(115200);
   ////INTERRUPT INIT/////////////////////////////////////////////////
   Timer1.initialize(2000);
   Timer1.attachInterrupt(updateDisplay);
-  attachInterrupt(digitalPinToInterrupt(MCP_INT), mcpInt, FALLING);
   attachInterrupt(digitalPinToInterrupt(BUTTON), btnInt, RISING);
   
   if(DEBUG) Serial.println("PRZERWANIA");
 //  MCP23017 INIT////////////////////////////////////////////////////
-  mcp.begin(7);
-  mcp.setupInterrupts(true,false,LOW);
+   mcp.begin(7);
   for(int i = 0; i <16;i++)
   {
     mcp.pinMode(i, INPUT);
-    mcp.setupInterruptPin(i,CHANGE);
   }
-
+//  mcp.writeRegister(0x02,B11111111);
+//  mcp.writeRegister(0x03,B11111111);
+previousStateOfSwitches=mcp.readGPIOAB();
   if(DEBUG) Serial.println("MCP23017 INIT");
   
   //SCT2024 INIT////////////////////////////////////////////////////
@@ -69,11 +93,11 @@ void setup()
   pinMode(DIGIT5,OUTPUT);
   pinMode(DIGIT6,OUTPUT);
 
-  pinMode(BUTTON, INPUT);
-  pinMode(MCP_INT, INPUT);
+  pinMode(BUTTON, INPUT_PULLUP);
+  pinMode(MCP_INT, INPUT_PULLUP);
 
-  pinMode(A0, INPUT);
-  pinMode(A3, INPUT);
+  pinMode(A0, INPUT_PULLUP);
+  pinMode(A3, INPUT_PULLUP);
 
   if(DEBUG) Serial.println("SCT2024 INIT");
 ///INIT VALUE FOR LED DRIVER//////////////////////////////////////////
@@ -82,38 +106,61 @@ void setup()
   digitalWrite(OE,HIGH);
 
   if(DEBUG) Serial.println("SCT2024 INIT VALUES");
-
+  updateSwitches();
 }
 
 void loop() 
 {
   if(DEBUG) Serial.println(stateMachine);
   if(DEBUG) Serial.println(mcp.readGPIOAB());
+
   
+if(timeToSleep >=2500)
+{
+  goToSleep();
+}
+
   updateStateMachine();
-  if(updateSwitchesFlag)
-  	{
-  		updateSwitches();
-  		updateSwitchesFlag = false;
-	}
+  if(previousStateOfSwitches!=mcp.readGPIOAB())
+  {
+    previousStateOfSwitches=mcp.readGPIOAB();
+    updateSwitches();
+    timeToSleep = 0;
+    
+  }
+
   if(stateMachine)
   	calculateHex();
   else
   	calculateBin();
+
+
 }
 
-void mcpInt()
+void goToSleep()
 {
-  if(DEBUG) Serial.println("mcpInt");
-  updateSwitchesFlag = true;
+    noInterrupts();
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  digitalWrite(DIGIT1,LOW); 
+  digitalWrite(DIGIT2,LOW);
+  digitalWrite(DIGIT3,LOW);
+  digitalWrite(DIGIT4,LOW);
+  digitalWrite(DIGIT5,LOW);
+  digitalWrite(DIGIT6,LOW);
+  sleep_enable(); 
+  interrupts();
+  sleep_mode();
+  timeToSleep=0;
+  sleep_disable();
+  
 }
-
 void updateStateMachine()
 {
   if(!btnState && prevBtnState)
   {
     prevBtnState = false;
     stateMachine++;
+    timeToSleep = 0;
   }
   if(btnState)
   {
@@ -125,13 +172,14 @@ void updateStateMachine()
 }
 void btnInt()
 {
-	btnState=false;
+  if(!(SMCR & B00000001)) //no change when sleep
+	  btnState=false;
 }
 void calculateBin()
 {
 	if(DEBUG) Serial.println("Calculate function");
    noInterrupts();
-   	  digits[0]=(value%1000000)/100000;
+   	  digits[0]=16;
       digits[1]=(value%100000)/10000;
   	  digits[2]=(value%10000)/1000;
   	  digits[3]=(value%1000)/100;
@@ -145,8 +193,8 @@ void calculateHex()
 {
 	if(DEBUG) Serial.println("Calculate function");
    noInterrupts();
-   	  digits[0]=(int)(value/pow(16,5))%16;
-  	  digits[1]=(int)(value/pow(16,4))%16;
+   	  digits[0]=17;
+  	  digits[1]=0;
   	  digits[2]=(int)(value/pow(16,3))%16;
   	  digits[3]=(int)(value/pow(16,2))%16;
   	  digits[4]=(value/16)%16;
@@ -157,61 +205,41 @@ void calculateHex()
 }
 void removeLeadingZeros()
 {
-	if(digits[0]+digits[1]+digits[2]+digits[3]+digits[4]==0) digits[4]=16;
-    if(digits[0]+digits[1]+digits[2]+digits[3]==0) digits[3]=16;
-    if(digits[0]+digits[1]+digits[2]==0) digits[2]=16;
-  	if(digits[0]+digits[1]==0) digits[1]=16;
-  	if(digits[0]==0) digits[0]=16;
+	if(digits[1]+digits[2]+digits[3]+digits[4]==0) digits[4]=16;
+  if(digits[1]+digits[2]+digits[3]==0) digits[3]=16;
+  if(digits[1]+digits[2]==0) digits[2]=16;
+  if(digits[1]==0) digits[1]=16;
 }
 
 void updateDisplay()
-{
-
-		switch (digitPosition) 
-		{
-		    case 1:
-		      printNumbers(digits[0],digits[3],1,0);
-		      break;
-		    case 2:
-		      printNumbers(digits[1],digits[4],2,0);
-		      break;
-		    case 3:
-		      printNumbers(digits[2],digits[5],3,0);
-		      break;
-		}
-		
+{	
+      switch (digitPosition) 
+    {
+        case 1:
+          printNumbers(digits[0],digits[3],1);
+          break;
+        case 2:
+          printNumbers(digits[1],digits[4],2);
+          break;
+        case 3:
+          printNumbers(digits[2],digits[5],3);
+          break;
+    }
 		if(++digitPosition == 4) digitPosition = 1;
+   timeToSleep++;
 }
 
-void printNumbers(int left, int right,int pos, bool dot)
+void printNumbers(int left, int right,int pos)
 {
-  // E,D,DP,C,G,A,F,B
-	bool lookup[17][8] =
-	   {{1,1,0,1,0,1,1,1}, //0
-		{0,0,0,1,0,0,0,1}, //1
-		{1,1,0,0,1,1,0,1}, //2
-		{0,1,0,1,1,1,0,1}, //3
-		{0,0,0,1,1,0,1,1}, //4
-		{0,1,0,1,1,1,1,0}, //5
-		{1,1,0,1,1,1,1,0}, //6
-		{0,0,0,1,0,1,0,1}, //7
-		{1,1,0,1,1,1,1,1}, //8
-		{0,1,0,1,1,1,1,1}, //9
-		{1,0,0,1,1,1,1,1}, //A
-		{1,1,0,1,1,0,1,0}, //B
-		{1,1,0,0,0,1,1,0}, //C
-		{1,1,0,1,1,0,0,1}, //D
-		{1,1,0,0,1,1,1,0}, //E
-		{1,0,0,0,1,1,1,0}, //F
-		{0,0,0,0,0,0,0,0}}; //NULL
+
 
 
 		digitalWrite(DIGIT1,LOW); 
-        digitalWrite(DIGIT2,LOW);
-        digitalWrite(DIGIT3,LOW);
-        digitalWrite(DIGIT4,LOW);
-        digitalWrite(DIGIT5,LOW);
-        digitalWrite(DIGIT6,LOW);
+    digitalWrite(DIGIT2,LOW);
+    digitalWrite(DIGIT3,LOW);
+    digitalWrite(DIGIT4,LOW);
+    digitalWrite(DIGIT5,LOW);
+    digitalWrite(DIGIT6,LOW);
 
   
 
@@ -260,10 +288,14 @@ void updateSwitches()
 {
   if(DEBUG) Serial.println("updateSwitches function");
   value=mcp.readGPIOAB();
-  int temp=value;
+  value=~value;
+
+  //reversing bits in value
+
+  uint16_t temp=value;
 
   value>>=1;
-  int i=15;
+  byte i=15;
   while(value)
   {
     temp <<=1;
@@ -273,6 +305,7 @@ void updateSwitches()
   }
   temp <<=i;
   value=temp;
+
   if(DEBUG) Serial.print("po odrwoceniu: ");
   if(DEBUG) Serial.println(value);
 
